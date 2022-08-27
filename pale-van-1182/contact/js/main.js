03a80ac1016d8618f0fb93eca922a2768e3b9b3e
Prankushh <107517914+Prankushh@users.noreply.github.com>
2022-08-27 09:14:19 +0530

    commit day 4
    still work in progress

---
+++ pale-van-1182/contact/js/main.js
@@ -0,0 +1,16 @@
+ 
+    document.querySelector(".login-btn").addEventListener("click",function(){
+        document.querySelector(".popup").style.display ="flex";
+    })
+    document.querySelector(".close").addEventListener("click",function(){
+        document.querySelector(".popup").style.display ="none";
+    })
+
+    document.querySelector(".login-btn").addEventListener("click",function(){
+        document.querySelector(".bg-modal").style.display ="flex    ";
+    })
+
+    document.querySelector(".close").addEventListener("click",function(){
+        document.querySelector(".bg-modal").style.display ="none";
+    })
+
